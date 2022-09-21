94059a87031bb482db4cae511d31d0019c1511df
jiehuaou <jiehua.ou@gmail.com>
2022-09-21 15:08:12 +0800

    java-stream

--- java-stream/src/main/java/demo/WordCountingFromFile.java
+++ java-stream/src/main/java/demo/WordCountingFromFile.java
@@ -4,15 +4,15 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.reducing;
-import static java.util.stream.Collectors.toMap;
-
+/**
+ * use nio to read in file
+ * use toMap to count word
+ */
 public class WordCountingFromFile {
     public static void main(String[] args) throws Exception {
 
